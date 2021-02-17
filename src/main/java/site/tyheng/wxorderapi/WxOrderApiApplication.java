5f0f3c224036ed19771d59b3fec529f628e2307e
Thomison <1021953309tyh@gmail.com>
2021-02-17 20:04:34 +0800

    创建项目

---
+++ src/main/java/site/tyheng/wxorderapi/WxOrderApiApplication.java
@@ -0,0 +1,13 @@
+package site.tyheng.wxorderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxOrderApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxOrderApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/site/tyheng/wxorderapi/WxOrderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package site.tyheng.wxorderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WxOrderApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
